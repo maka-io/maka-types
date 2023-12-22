7c55b97464255580e2fe6b459c0815e079609317
maka-io <mjmcee@gmail.com>
2023-12-22 10:46:06 -0500

    README

---
+++ README.md
@@ -0,0 +1,11 @@
+# @maka/types
+
+#### TypeScript Definitions for `@maka` Meteor Packages!
+
+## Installation
+
+You can install @maka/types using npm:
+
+```bash
+npm install @maka/types --save-dev
+```
---
+++ src/meteor/maka-http.d.ts
@@ -0,0 +1,79 @@
+declare module 'meteor/maka:http' {
+  /**
+   * Namespace for HTTP functionalities on the server.
+   */
+  namespace HTTPServer {
+    /**
+     * Represents the options for an HTTP call on the server.
+     * @typeparam T The type of the `data` field in the options, default is `any`.
+     */
+    interface Options<T = any> {
+      data?: T;
+      params?: { [key: string]: any };
+      headers?: { [key: string]: string };
+      followRedirects?: boolean;
+    }
+
+    /**
+     * Performs an HTTP call on the server.
+     * @param method The HTTP method (e.g., 'GET', 'POST').
+     * @param url The URL to which the request is sent.
+     * @param options The options for the HTTP request.
+     * @returns A promise that resolves with the HTTP response.
+     */
+    function call(
+      method: string,
+      url: string,
+      options?: HTTPServer.Options,
+    ): Promise<HTTPCommon.HTTPResponse>;
+  }
+
+  /**
+   * Namespace for HTTP functionalities on the client.
+   */
+  namespace HTTPClient {
+    /**
+     * Represents the options for an HTTP call on the client.
+     * @typeparam T The type of the `data` field in the options, default is `any`.
+     */
+    interface Options<T = any> {
+      data?: T;
+      params?: { [key: string]: any };
+      headers?: { [key: string]: string };
+    }
+
+    /**
+     * Performs an HTTP call on the client.
+     * @param method The HTTP method (e.g., 'GET', 'POST').
+     * @param url The URL to which the request is sent.
+     * @param options The options for the HTTP request.
+     * @returns A promise that resolves with the HTTP response.
+     */
+    function call(
+      method: string,
+      url: string,
+      options?: HTTPClient.Options,
+    ): Promise<HTTPCommon.HTTPResponse>;
+  }
+
+  /**
+   * Common namespace for HTTP functionalities shared by both server and client.
+   */
+  namespace HTTPCommon {
+    /**
+     * Represents the HTTP response returned by an HTTP call.
+     * @typeparam T The type of the `data` field in the response, default is `any`.
+     */
+    interface HTTPResponse<T = any> {
+      statusCode: number;
+      headers: { [id: string]: string };
+      data: T;
+    }
+
+    // Documentation for del, get, post, put follows a similar pattern.
+    function del<T = any>(url: string, callOptions?: HTTPServer.Options | HTTPClient.Options): Promise<HTTPCommon.HTTPResponse<T>>;
+    function get<T = any>(url: string, callOptions?: HTTPServer.Options | HTTPClient.Options): Promise<HTTPCommon.HTTPResponse<T>>;
+    function post<T = any>(url: string, callOptions?: HTTPServer.Options | HTTPClient.Options): Promise<HTTPCommon.HTTPResponse<T>>;
+    function put<T = any>(url: string, callOptions?: HTTPServer.Options | HTTPClient.Options): Promise<HTTPCommon.HTTPResponse<T>>;
+  }
+}
